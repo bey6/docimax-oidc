8a3cb07336e8b4704170f561febd42225b944a0f
bei <liub@docimax.com.cn>
2019-11-29 10:47:18 +0800

    add userGetter, & permissionGetter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import { oidcRoute } from './service/oidc-route'
-import { createOidcUserManager } from './service/oidc-helpers'
-import OidcStore from './service/oidc-store'
-import pkg from '../package.json'
+import { oidcRoute } from './service/oidc-route';
+import { createOidcUserManager } from './service/oidc-helpers';
+import OidcStore from './service/oidc-store';
+import pkg from '../package.json';
 
 // setup
 export const oidcInstance = {
   install (Vue, options) {
     // show version
-    console.log(`@docimax/oidc version: ${pkg.version}`)
+    console.log(`@docimax/oidc version: ${pkg.version}`);
     // oidc user manager
-    Vue.prototype.$um = createOidcUserManager(options.oidcConf)
+    Vue.prototype.$um = createOidcUserManager(options.oidcConf);
     // 认证地址
-    Vue.prototype.$authUrl = options.oidcConf.authority
+    Vue.prototype.$authUrl = options.oidcConf.authority;
     // 添加回调路由
-    options.router.addRoutes(oidcRoute)
+    options.router.addRoutes(oidcRoute);
     // 注册模块
-    options.store.registerModule('docimax', OidcStore)
+    options.store.registerModule('docimax', OidcStore);
   }
-}
+};
--- src/service/oidc-store.js
+++ src/service/oidc-store.js
@@ -6,9 +6,35 @@ export default {
   },
   getters: {
     getUserinfo: state => {
+      if (state.userinfo && state.userinfo.Name) {
+        return state.userinfo;
+      }
+      let userinfoStr = window.sessionStorage.getItem('@docimax/oidc:userinfo');
+      state.userinfo = JSON.parse(userinfoStr);
       return state.userinfo;
     },
     getPermission: state => {
+      if (state.permission && state.permission.User && state.permission.User.Name) {
+        return state.permission;
+      }
+      let permissionStr = window.sessionStorage.getItem('@docimax/oidc:permission');
+      state.permission = JSON.parse(permissionStr);
+      return state.permission;
+    },
+    userGetter: state => {
+      if (state.userinfo && state.userinfo.Name) {
+        return state.userinfo;
+      }
+      let userinfoStr = window.sessionStorage.getItem('@docimax/oidc:userinfo');
+      state.userinfo = JSON.parse(userinfoStr);
+      return state.userinfo;
+    },
+    permissionGetter: state => {
+      if (state.permission && state.permission.User && state.permission.User.Name) {
+        return state.permission;
+      }
+      let permissionStr = window.sessionStorage.getItem('@docimax/oidc:permission');
+      state.permission = JSON.parse(permissionStr);
       return state.permission;
     }
   }
